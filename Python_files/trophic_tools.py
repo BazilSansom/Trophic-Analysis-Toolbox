c3d38d8548ba6d5c3b2ee416e05b66ebd7c525a7
BazilSansom <bazilsansom@gmail.com>
2021-03-01 21:38:01 +0000

    Update trophic_tools.py

--- Python_files/trophic_tools.py
+++ Python_files/trophic_tools.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[57]:
-
-
 # This Python module is for conductin trophic analysis as introduced by [1], and 
 # also provides tools developed for network visualisation using these methods.
 # Equation numbers refered to in annotation are from [1].
@@ -241,18 +235,6 @@ def trophic_plot(G,
     limits=plt.axis('on') # turns on axis
     ax.tick_params(left=True, labelleft=True)
     plt.ylabel('Trophic Levels')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
