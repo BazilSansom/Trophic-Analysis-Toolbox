e4fed17ee02b142f4bc0d0a5314bdd65a5d073d7
Bazil Sansom <bazilsansom@gmail.com>
2021-07-01 07:16:56 +0100

    Update standard_levels.m
    Updating references and standardising notation across functions

--- Matlab_files/standard_levels.m
+++ Matlab_files/standard_levels.m
@@ -1,23 +1,24 @@
-function [s] = standard_levels(adj_mtrx)
+function [s] = standard_levels(W)
 % This function calculates standard trophic levels (Eq.2 [1]) 
 % according to [2].
 %
 % INPUTS:
-%  A  a (possibley weighted) adjacency matrix
+%  W  a (possibly weighted) adjacency matrix
 %
 % OUTPUTS:
 %  s   a vector of trophic levels according to standard definition
-%      (corresponding to Eq.2 [1]).
+%      (corresponding to solution to Eq.4.2 [1]).
 
 % REFERENCES
-% [1] Mackay, Johnson and Sansom ...
+% [1] MacKay RS, Johnson S, Sansom B. 2020 How directed is a directed network? 
+%       R. Soc. Open Sci. 7: 201138. http://dx.doi.org/10.1098/rsos.201138
 % [2] Levine (1980) JTB Vol 83
 %
 % Function beggins
 
 % Construct v
-nNodes=size(adj_mtrx,1); % number of nodes
-k_in=sum(adj_mtrx,1);    % vector of in-degrees
+nNodes=size(W,1); % number of nodes
+k_in=sum(W,1);    % vector of in-weights
 
 %v=zeros(nNodes,1);% pre-allocating
 %for i=1:nNodes 
@@ -25,18 +26,16 @@ k_in=sum(adj_mtrx,1);    % vector of in-degrees
 %end
 
 v=max([k_in',ones(nNodes,1)],[],2); % basal nodes have level 1 by convention
-%Lambda=diag(v)-transpose(adj_mtrx);
-Lambda=diag(v)-adj_mtrx';
-%Lambda=diag(v)-adj_mtrx;
+Lambda=diag(v)-W'; % Laplacian
 
 if det(Lambda)==0
     %fprintf('Error: Lambda is a singular matrix.\n  Network must have at lease one basal node to obtain standard trophic levels.\n Consider using new levels.')
-    error('Error: Lambda is a singular matrix.\n  Network must have at lease one basal node to obtain standard trophic levels.\n Consider using new levels.')
+    error('Error: Lambda is a singular matrix.\n  Network must have at lease one basal node to obtain standard trophic levels.\n Consider using improved levels.')
     s=[];
     % 
     return
 else
-    % Solve Ls=v for s
+    % Solve Ls=v for s (Eq.4.2 [1])
     %s=(Lambda^-1)*v; 
     s=Lambda\v;
 end
